503ad524bcf6c6eea10991d27b050aa374ab91de
Richard Pascarelli <Rpavesi84@gmail.com>
2019-11-11 12:37:04 -0500

    initial mvc setup and migration complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,39 @@
 Rails.application.routes.draw do
+  get 'genres/index'
+
+  get 'genres/show'
+
+  get 'genres/new'
+
+  get 'genres/create'
+
+  get 'genres/edit'
+
+  get 'genres/update'
+
+  get 'artists/index'
+
+  get 'artists/show'
+
+  get 'artists/new'
+
+  get 'artists/create'
+
+  get 'artists/edit'
+
+  get 'artists/update'
+
+  get 'songs/index'
+
+  get 'songs/show'
+
+  get 'songs/new'
+
+  get 'songs/create'
+
+  get 'songs/edit'
+
+  get 'songs/update'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
